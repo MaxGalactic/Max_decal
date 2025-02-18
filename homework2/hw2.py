466d93cbccf452ff2ce73e9a8ed0c7eeb23bd555
Max Velasquez <maxvelasquez@Maxs-MacBook-Air.local>
2025-02-17 20:11:39 -0800

    Adding Homework2

--- homework2/hw2.py
+++ homework2/hw2.py
@@ -1,10 +1,20 @@
 # 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+# You can do the command cd or cd ../../ to get back to desktop from python_decal. 
 # 2) What does ~/ mean?
+# ~/ is the home directory.
 # 3) What's the difference between and abolute and a relative path in your terminal?
+# An absolute path is a path that starts from your desktop directory, and a relative path is relative to your current working directory. 
 # 4) What command would bring you back to your home directory?
+# cd ~ would bring you back to the home directory.
 # 5) If you called rm ./ in your current working directory, what would it do? 
+# It would say rm: "." and ".." may not be removed as it refers to your current directory and you cant delete what you're currently inside.
 # 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
-# 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+# Git add moves files from the working directory to the staging area. Git commit saves what you added form git added to the local repository. Git Push uploads your commits (code) to git hub from your computer.
+# 7)Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+# Git add will move the file to the staging area and git status shows which files where changes are ready to be commited. 
 # 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+# Forgetting my repository password, then having to delete it and make a new one... twice.
 # 9) How does cloning a repository differ from pulling from a repository?
+# cloning creates a copy of a remote repository and pulling from a repository updates your local repository with changes from a remote repository on the internet.
 # 10) Tell me a fun fact!
+# I like googly eyes and also collecting vinyls :D .
